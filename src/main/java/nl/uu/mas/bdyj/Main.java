149e9dca2e3c6e172fdb0d4399a3292de97370de
Jappie Klooster <jappieklooster@hotmail.com>
2016-02-10 22:41:05 +0100

    it builds!

--- src/main/java/nl/uu/mas/bdyj/Main.java
+++ src/main/java/nl/uu/mas/bdyj/Main.java
@@ -1,6 +1,7 @@
-package nl.jappieklooster.hw.ms;
+package nl.uu.mas.bdyj;
 
 import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 class Main{
 	private static final Logger log = LoggerFactory.getLogger(Main.class);
