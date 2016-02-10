bd65be058947928059bac825243d4d848a1253d3
Jappie Klooster <jappieklooster@hotmail.com>
2016-02-10 20:25:11 +0100

    INIT

---
+++ src/main/java/nl/uu/mas/bdyj/Main.java
@@ -0,0 +1,11 @@
+package nl.jappieklooster.hw.ms;
+
+import org.slf4j.LoggerFactory;
+
+class Main{
+	private static final Logger log = LoggerFactory.getLogger(Main.class);
+	public static void main(String[] args) {
+		log.info("the JADI returns!");
+	}
+
+}
