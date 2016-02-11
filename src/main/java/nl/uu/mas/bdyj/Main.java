9db43233bd8ee70223652a177614674b4084dfea
Jappie Klooster <jappieklooster@hotmail.com>
2016-02-11 19:04:13 +0100

    It opens a GUI now

--- src/main/java/nl/uu/mas/bdyj/Main.java
+++ src/main/java/nl/uu/mas/bdyj/Main.java
@@ -1,5 +1,8 @@
 package nl.uu.mas.bdyj;
 
+import jade.Boot;
+import jade.core.Agent;
+import jade.domain.JADEAgentManagement.CreateAgent;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
@@ -7,6 +10,14 @@ class Main{
 	private static final Logger log = LoggerFactory.getLogger(Main.class);
 	public static void main(String[] args) {
 		log.info("the JADI returns!");
+		final Agent a = new Agent(){
+			@Override
+			protected void setup() {
+				super.setup();
+				log.info("agent reporting");
+			}
+		};
+		Boot.main(new String[]{"-gui"});
 	}
 
 }
