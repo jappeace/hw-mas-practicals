2241d9b8bdc668d4036f8ba96213823d1f4f89e0
Jappie Klooster <jappieklooster@hotmail.com>
2016-02-12 15:23:17 +0100

    this should work, but the jess library is missing

--- src/main/java/nl/uu/mas/bdyj/Main.java
+++ src/main/java/nl/uu/mas/bdyj/Main.java
@@ -1,10 +1,15 @@
 package nl.uu.mas.bdyj;
 
+import examples.bookTrading.BookSellerAgent;
+import examples.bookTrading.BookSellerGui;
+import examples.jess.BasicJessBehaviour;
 import jade.Boot;
 import jade.core.Agent;
 import jade.domain.JADEAgentManagement.CreateAgent;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+
+import java.util.Random;
 
 class Main{
 	private static final Logger log = LoggerFactory.getLogger(Main.class);
@@ -17,7 +22,8 @@ class Main{
 				log.info("agent reporting");
 			}
 		};
-		Boot.main(new String[]{"-gui"});
+		Boot.main(new String[]{"-gui -local-port 1100 jade.Boot;buyer1:examples.bookTrading.BookBuyerAgent(book1,book2,book3);seller1:examples.bookTrading.BookSellerAgent(book2,book3,book4)"});
+
 	}
 
 }
