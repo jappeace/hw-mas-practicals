a23524b52b0550d53aeda2acf15b847ada4930e0
Jappie Klooster <jappieklooster@hotmail.com>
2016-03-18 13:38:48 +0100

    I have no idea where these came from...

--- src/main/java/Circumference.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Circumference {
-    public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-
-        // Program your solution here 
-    }
-}
