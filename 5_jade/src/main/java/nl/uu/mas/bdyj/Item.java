cfd0eeb0f248f7e8aa53f9e36b0c5d3997ef0c5f
Jappie Klooster <jappieklooster@hotmail.com>
2016-04-05 15:59:57 +0200

    added some decoration to the item class

--- 5_jade/src/main/java/nl/uu/mas/bdyj/Item.java
+++ 5_jade/src/main/java/nl/uu/mas/bdyj/Item.java
@@ -2,8 +2,29 @@ package nl.uu.mas.bdyj;
 
 public class Item {
 	public String name;
+	public Color color;
 	public Item(String name){
 		this.name = name;
 	}
-
+	enum Color{
+		Black,
+		White,
+		Green,
+		Aqua
+	}
+	public Material material;
+	enum Material{
+		Gold,
+		Copper,
+		Wood,
+		Silver
+	}
+	public int weight;
+	public Item copy(){
+		Item result = new Item(name);
+		result.color = color;
+		result.material = material;
+		result.weight = weight;
+		return result;
+	}
 }
